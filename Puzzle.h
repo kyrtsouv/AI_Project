98b92693257b54a41cc189839f57e03f9df4c6e3
kyrtsouv <kyrtsouv@csd.auth.gr>
2023-05-01 23:28:16 +0300

    Final

--- Puzzle.h
+++ Puzzle.h
@@ -2,6 +2,7 @@
 #define PUZZLE_H
 
 #include <vector>
+#include <string>
 
 #include "Car.h"
 
@@ -10,16 +11,18 @@ using namespace std;
 class Puzzle
 {
 private:
-    bool **free;
     int width;
     int height;
-    string key;
-    Puzzle *previous;
-    vector<Car> cars;
     vector<pair<int, int>> objects;
 
+    bool **free;
+    string key;
+    vector<Car> cars;
+
+    Puzzle *previous;
+
 public:
-    Puzzle(vector<Car> cars, vector<pair<int, int>> objects, int m, int n, Puzzle *previous = NULL);
+    Puzzle(vector<Car> cars, vector<pair<int, int>> objects, int m, int n, Puzzle *previous = nullptr);
 
     void setCars(vector<Car> cars) { this->cars = cars; }
     void setPrevious(Puzzle *p) { previous = p; }
@@ -35,6 +38,8 @@ public:
     bool goLeft(Puzzle *&puzzle, int carIndex);
     bool goUp(Puzzle *&puzzle, int carIndex);
     bool goDown(Puzzle *&puzzle, int carIndex);
+
+    bool operator==(const Puzzle &puzzle) const { return key == puzzle.key; }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -33,10 +33,10 @@ Puzzle *BFS(Puzzle *puzzle, long long &mem, long long &examined)
             examined++;
             closed.insert({current->getKey(), current});
             vector<Puzzle *> children = current->expand();
-            for (int i = 0; i < children.size(); i++)
+            for (Puzzle *child : children)
             {
-                if (closed.count(children[i]->getKey()) == 0)
-                    agenda.push(children[i]);
+                if (closed.count(child->getKey()) == 0)
+                    agenda.push(child);
             }
         }
     }
@@ -46,7 +46,7 @@ Puzzle *BFS(Puzzle *puzzle, long long &mem, long long &examined)
 int main()
 {
 
-    int TEST = 2;
+    int TEST = 3;
 
     long long mem, examined;
 
@@ -56,10 +56,10 @@ int main()
     if (TEST == 1)
     {
         width = 4;
-        height = 3;
-        cars = {{2, 0}, {2, 1, false}, {1, 1}};
+        height = 4;
+        cars = {{2, 0}, {2, 1, false}, {1, 1}, {1, 2, false}};
         objects = {
-            {0, 1}, {2, 2}, {3, 0}};
+            {0, 1}, {2, 2}, {3, 0}, {1, 3}};
     }
     if (TEST == 2)
     {
@@ -73,7 +73,7 @@ int main()
     {
         width = 6;
         height = 6;
-        cars = {{4, 1, false}, {5, 1}, {0, 3, false}, {2, 4}, {3, 4}, {3, 5, false}, {4, 5, false}};
+        cars = {{4, 1, false}, {5, 1}, {0, 3, false}, {2, 4}, {3, 4}, {4, 5, false}};
         objects = {{1, 1}, {5, 3}, {0, 4}};
     }
 
